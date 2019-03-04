90cbeef7a0a411519ac7ddfbc2843046daaca802
Richard Iannone <riannone@me.com>
2019-03-03 23:37:41 -0500

    Add roxygen example

--- R/open_url.R
+++ R/open_url.R
@@ -2,6 +2,10 @@
 #'
 #' @param url The URL that we want to view.
 #' @param browser The name of the web browser.
+#' @examples
+#' # Open the RStudio website in a new Safari tab
+#' open_url("http://www.rstudio.com")
+#'
 #' @importFrom processx run
 #' @export
 open_url <- function(url,
